d0277ede202fa1d86d0c7e66d81d76a3d32ecfb7
Kyle-Venegas <venegas.kyle@gmail.com>
2020-07-17 16:04:04 -0700

    parses all album songs

--- lyrics.py
+++ lyrics.py
@@ -7,27 +7,20 @@ def ask():
     song = input('Song name?: ')
     artist_song = artist + ' ' + song
     full = artist_song.replace(' ', '-')
-    url = 'https://genius.com/' + full + '-lyrics'
-    return url
+    song_url = 'https://genius.com/' + full + '-lyrics'
+    return song_url
 
-url_list = []
-def new_ask():
+def album_ask():
     artist = input('Artist name?: ')
-    tracks = int(input('How many tracks?: '))
-    for track in range(tracks):
-        song = input('song name?: ')
-        artist_song = artist + ' ' + song
-        full = artist_song.replace(' ', '-')
-        url = 'https://genius.com/' + full + '-lyrics'
-        url_list.append(url)
+    album = input('Album name?: ')
+    album_url = 'https://genius.com/albums/' + artist.replace(' ','-') + '/' + album.replace(' ','-')
+    return album_url
 
-    return url_list
-
-class lyricSpider(scrapy.Spider):
+class song_Spider(scrapy.Spider):
     name = 'lyric'
 
+    start_urls = [album_ask()]
 #   start_urls = [ask()]
-    start_urls = new_ask()
 
     custom_settings = {
             'LOG_ENABLED' : False,
@@ -35,17 +28,24 @@ class lyricSpider(scrapy.Spider):
             }
 
     def parse(self, response):
+        links = response.css('a.u-display_block ::attr(href)').getall()
+        prio = len(links)
+        for link in links:
+            yield scrapy.Request(link, self.parse_lyrics, priority = prio)
+            prio -= 1
+
+    def parse_lyrics(self, response):
         os.system('clear')
-        h1 = (response.css('h1::text').get())
-        h2 = (response.css('h2 a::text').get())
+        h1 = response.css('h1::text').get()
+        h2 = response.css('h2 a::text').get()
         print(h2, ' - ', h1, '\n')
-        for line in response.css('div.lyrics ::text').getall()[2:]:
+        for line in response.css('div.lyrics ::text').getall()[2:-2]:
             print(line, end='')
         delay = input('')
 
 def lyric():
     process = CrawlerProcess()
-    process.crawl(lyricSpider)
+    process.crawl(song_Spider)
     process.start()
 
 if __name__ == '__main__':
