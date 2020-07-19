ee6fee83a490ac2e01e25b4c49be459b4e8d5d26
Kyle-Venegas <venegas.kyle@gmail.com>
2020-07-18 23:42:09 -0700

    single song spider

---
+++ song.py
@@ -0,0 +1,29 @@
+import scrapy
+from scrapy.crawler import CrawlerProcess
+import os
+
+def ask():
+    artist = input('Artist name?: ')
+    song = input('Song name?: ')
+    artist_song = artist + ' ' + song
+    full = artist_song.replace(' ', '-')
+    url = 'https://genius.com/' + full + '-lyrics'
+    return url
+
+class lyricSpider(scrapy.Spider):
+    name = 'lyric'
+
+    start_urls = [ask()]
+
+    custom_settings = {
+            'LOG_ENABLED' : False
+            }
+
+    def parse(self, response):
+        os.system('clear')
+        for line in response.css('div.lyrics ::text').getall()[2:-2]:
+            print(line, end='')
+
+process = CrawlerProcess()
+process.crawl(lyricSpider)
+process.start()
